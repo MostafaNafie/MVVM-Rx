ee80984e5433f89e3b5e02ecec574eb3467ac2de
Mostafa Nafie <MostafaTarekNafie@gmail.com>
2020-02-16 17:14:55 +0200

    Restructure project files to MVVM

--- MVVM+Rx/View/MainViewController.swift
+++ MVVM+Rx/View/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  MVVM+Rx
 //
 //  Created by Mustafa on 16/2/20.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
---
+++ MVVM+Rx/ViewModel/MainViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewModel.swift
+//  MVVM+Rx
+//
+//  Created by Mustafa on 16/2/20.
+//  Copyright © 2020 Mustafa Nafie. All rights reserved.
+//
+
+import Foundation
+
+class MainViewModel {
+	
+	
+	
+}
